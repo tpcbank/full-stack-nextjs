129371934f1f13ab861146d4b20bbfdd600c9de1
tpcbankk <tanapurached.pra@gmail.com>
2024-01-31 19:46:33 +0700

    handle error

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,15 +3,15 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createIssuesSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required."),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssuesSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssues = await prisma.issues.create({
     data: { title: body.title, description: body.description },
